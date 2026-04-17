248e30268d7650ebf1e23bb38e331070cb55d672
Rufio244 <thanvas9991@gmail.com>
2026-04-17 08:33:52 +0700

    Update police_api.py

--- app/services/police_api.py
+++ app/services/police_api.py
@@ -1,1 +1,9 @@
+import requests
+from config import settings
 
+def send_to_police(case_data):
+    try:
+        res = requests.post(settings.POLICE_API, json=case_data)
+        return res.json()
+    except:
+        return {"status": "mock_sent"}
