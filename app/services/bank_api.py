5b287d9147248d92a257d08e245b256ef9151ebc
Rufio244 <thanvas9991@gmail.com>
2026-04-17 08:33:25 +0700

    Update bank_api.py

--- app/services/bank_api.py
+++ app/services/bank_api.py
@@ -1,1 +1,20 @@
+import requests
+from config import settings
 
+def freeze_account(bank, account_no, case_id):
+    url_map = {
+        "KBANK": settings.KBANK_API,
+        "SCB": settings.SCB_API
+    }
+    
+    payload = {
+        "account": account_no,
+        "reason": "SCAM",
+        "case_id": case_id
+    }
+    
+    try:
+        res = requests.post(url_map[bank], json=payload)
+        return res.json()
+    except:
+        return {"status": "mock_freeze_success"}
