53e94d4b9ba91bd19a74ea2e7d0a5624c512db4b
Rufio244 <thanvas9991@gmail.com>
2026-04-17 08:32:17 +0700

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,1 +1,14 @@
+from sqlalchemy import Column, String, Integer
+from database import Base
 
+class Case(Base):
+    __tablename__ = "cases"
+    
+    id = Column(String, primary_key=True)
+    bank_case_id = Column(String)
+    citizen_id = Column(String)
+    phone = Column(String)
+    status = Column(String)
+    scam_type = Column(String)
+    risk_score = Column(String)
+    assigned_station = Column(String)
