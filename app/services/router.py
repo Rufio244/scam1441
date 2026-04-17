0c4fac953e577892aa2fb40debd4ebccfaf54f1b
Rufio244 <thanvas9991@gmail.com>
2026-04-17 08:34:20 +0700

    Update router.py

--- app/services/router.py
+++ app/services/router.py
@@ -1,1 +1,6 @@
-
+def route_case(citizen_id):
+    if citizen_id.startswith("5"):
+        return "สถานีตำรวจ ลำปาง"
+    elif citizen_id.startswith("1"):
+        return "สถานีตำรวจ กรุงเทพ"
+    return "สถานีตำรวจ ภูมิภาค"
